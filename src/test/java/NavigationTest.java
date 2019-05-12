fe75a31025aef0f45efbf88e878918a27b0726a9
Rahul <rahulahuja404@gmail.com>
2019-05-12 23:29:32 +0530

    refactor keywords

--- src/test/java/NavigationTest.java
+++ src/test/java/NavigationTest.java
@@ -22,16 +22,18 @@ public class NavigationTest {
         while (rowIterator.hasNext()){
             Iterator<Cell> cellIterator = rowIterator.next().cellIterator();
             String command = cellIterator.next().getStringCellValue().toLowerCase();
+            System.out.println("command = "+command);
             if (command.equals("goto")){
                 String url = cellIterator.next().getStringCellValue();
+                System.out.println("url = "+url);
                 webDriver.get(url);
                 Assert.assertEquals(webDriver.getCurrentUrl(), url);
                 Assert.assertEquals(webDriver.getTitle().toLowerCase(), cellIterator.next().getStringCellValue().toLowerCase());
             }
-            else if (command.equals("fill")){
-                while (cellIterator.hasNext()){
-                    String[] formValue = cellIterator.next().getStringCellValue().split(" ");
-                    webDriver.findElement(By.name(formValue[0])).sendKeys(formValue[1]);
+            else if (command.equals("type")){
+                String findBy = cellIterator.next().getStringCellValue().toLowerCase();
+                if (findBy.equals("name")){
+                    webDriver.findElement(By.name(cellIterator.next().getStringCellValue())).sendKeys(cellIterator.next().getStringCellValue());
                 }
             }
             else if (command.equals("click")){
